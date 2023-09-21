df3c7d6f142cdfb8f90ea9ed5df21ce13ac41a22
Zhiyuan Liu <51340403+Liu-ZY-98@users.noreply.github.com>
2023-09-21 14:35:06 -0700

    Changed character to circle, maximum two splits

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target == null) return;
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,16 @@ public class PlayerController : MonoBehaviour
     public bool isJumping;
 
     private Rigidbody2D rb;
+    
+    public enum ColorType { None, Blue, Green, Purple, Red }
+    public ColorType color1 = ColorType.None;
+    public ColorType color2 = ColorType.None;
+
+    public GameObject playerPrefab;
+    public CameraController cameraController;
+    
+    private GameObject splitObject;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +31,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Horizontal movement
         movement = Input.GetAxis("Horizontal");
-
         rb.velocity = new Vector2(speed * movement, rb.velocity.y);
 
+        // Jump and its conditions
         if (Input.GetButtonDown("Jump") && isJumping == false)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
         }
+        
+        // Press "J" to SPLIT when the player has two colors
+        if (Input.GetKeyDown(KeyCode.N) && color1 != ColorType.None && color2 != ColorType.None)
+            Split();
+        
+        // Press "K" to MERGE when there are two balls present
+        if (Input.GetKeyDown(KeyCode.M) && splitObject != null)
+            Merge();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -38,12 +57,26 @@ public class PlayerController : MonoBehaviour
             isJumping = false;
         }
     }
-
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
             isJumping = true;
         }
+    }
+    
+    void Split()
+    {
+        splitObject = Instantiate(playerPrefab, transform.position + new Vector3(2, 0, 0), Quaternion.identity);
+        splitObject.GetComponent<PlayerController>().color1 = color2;
+        splitObject.GetComponent<PlayerController>().color2 = ColorType.None;
+        color2 = ColorType.None;
+    }
+
+    void Merge()
+    {
+        color2 = splitObject.GetComponent<PlayerController>().color1;
+        Destroy(splitObject);
+        splitObject = null;
     }
 }
