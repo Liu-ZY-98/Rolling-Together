fbab8494a13ee84e8bcf0426abe95990edfc054b
likhitha99 <ch.likhitha22@gmail.com>
2023-09-23 22:57:24 +0530

    Implement split and merge.

--- Assets/Scripts/PurpleRepel.cs
+++ Assets/Scripts/PurpleRepel.cs
@@ -14,14 +14,12 @@ public class PurpleRepel : MonoBehaviour
     {
         gameManager = FindObjectsOfType<GameManager>()[0];
     }
-    
-    // Update is called once per frame
+
     void Update()
     {
-        // int layerMask = 1 << 6;
         Vector2 center = transform.position;
         Vector2 size = new Vector2(3f, 3f);
-        RaycastHit2D[] hits = Physics2D.BoxCastAll(center, size, 0f, Vector2.up, 2f);
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(center, size, 0f, Vector2.up, 3f);
         foreach (RaycastHit2D hit in hits)
         {
             if (hit.collider != null && hit.collider.gameObject.CompareTag("Player") && gameManager.GetPlayerColor() == GameManager.PlayerColor.PURPLE)
