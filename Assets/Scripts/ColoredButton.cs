897b658a90b563eb5f11bc7eaf52658b1935f882
likhitha99 <ch.likhitha22@gmail.com>
2023-09-24 12:38:41 +0530

    Add red-blue gameplay.

---
+++ Assets/Scripts/ColoredButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColoredButton : MonoBehaviour
+{
+    public GameManager.PlayerColor ActivatorColor;
+
+    public bool IsPressed;
+
+    public GameObject UnpressedButton;
+
+    public GameObject PressedButton;
+
+    public UnityEvent Action;
+
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        gameManager = FindObjectsOfType<GameManager>()[0];
+        if (IsPressed)
+        {
+            UnpressedButton.SetActive(false);
+            PressedButton.SetActive(true);
+        }
+        else
+        {
+            UnpressedButton.SetActive(true);
+            PressedButton.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!IsPressed && other.gameObject.CompareTag("Player") && gameManager.GetPlayerColor() == ActivatorColor)
+        {
+            IsPressed = true;
+            UnpressedButton.SetActive(false);
+            PressedButton.SetActive(true);
+            Action.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/ColoredPlatform.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredPlatform : MonoBehaviour
+{
+    public GameManager.PlayerColor AllowedColor;
+
+    private GameManager gameManager;
+
+    void Awake()
+    {
+        gameManager = FindObjectsOfType<GameManager>()[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameManager.GetPlayerColor() != AllowedColor)
+        {
+            Destroy(other.gameObject);
+            gameManager.EndGame();
+        }
+    }
+}
